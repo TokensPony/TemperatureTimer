5d3912cd0964d1063f405befb8a68c10ed6c219d
TokensPony <cmtherealmccoy@gmail.com>
2019-11-17 21:40:15 -0600

    Combined old layout with BBC Micro Bit Code

---
+++ app/src/main/java/com/bluetooth/mwoolley/microbitbledemo/ui/TemperatureActivity.java
@@ -0,0 +1,243 @@
+package com.bluetooth.mwoolley.microbitbledemo.ui;
+/*
+ * Author: Martin Woolley
+ * Twitter: @bluetooth_mdw
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bluetooth.mwoolley.microbitbledemo.bluetooth.BleAdapterService;
+import com.bluetooth.mwoolley.microbitbledemo.bluetooth.ConnectionStatusListener;
+import com.bluetooth.mwoolley.microbitbledemo.Constants;
+import com.bluetooth.mwoolley.microbitbledemo.MicroBit;
+import com.bluetooth.mwoolley.microbitbledemo.R;
+import com.bluetooth.mwoolley.microbitbledemo.Utility;
+
+public class TemperatureActivity extends AppCompatActivity implements ConnectionStatusListener {
+
+    private BleAdapterService bluetooth_le_adapter;
+
+    private int exit_step = 0;
+    private boolean exiting = false;
+
+    private boolean fahr = true;
+
+    private RadioGroup tempButtons;
+    private RadioButton fahrButton;
+    private RadioButton celButton;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            Log.d(Constants.TAG, "onServiceConnected");
+            bluetooth_le_adapter = ((BleAdapterService.LocalBinder) service).getService();
+            bluetooth_le_adapter.setActivityHandler(mMessageHandler);
+
+            if (bluetooth_le_adapter.setNotificationsState(Utility.normaliseUUID(BleAdapterService.TEMPERATURESERVICE_SERVICE_UUID), Utility.normaliseUUID(BleAdapterService.TEMPERATURE_CHARACTERISTIC_UUID), true)) {
+                showMsg(Utility.htmlColorGreen("Temperature notifications ON"));
+            } else {
+                showMsg(Utility.htmlColorRed("Failed to set Temperature notifications ON"));
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        setContentView(R.layout.activity_temperature);
+
+        exiting = false;
+
+        // read intent data
+        final Intent intent = getIntent();
+        MicroBit.getInstance().setConnection_status_listener(this);
+
+        // connect to the Bluetooth smart service
+        Intent gattServiceIntent = new Intent(this, BleAdapterService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+        tempButtons = (RadioGroup) findViewById(R.id.tempButtons);
+        fahrButton = (RadioButton) findViewById(R.id.fahrTemp);
+        celButton = (RadioButton) findViewById(R.id.celTemp);
+
+        tempButtons.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.fahrTemp:
+                        fahr = true;
+                        Log.i("fahr", "Fahrenheit Selected");
+                        Toast.makeText(TemperatureActivity.this, "Fahrenheit Selected",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.celTemp:
+                        fahr = false;
+                        Log.i("cel", "Celcius selected");
+                        Toast.makeText(TemperatureActivity.this, "Celcius Selected",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            // may already have unbound. No API to check state so....
+            unbindService(mServiceConnection);
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(Constants.TAG, "onBackPressed");
+        shutdownSteps();
+    }
+
+    private void shutdownSteps() {
+        exiting=true;
+        exit_step++;
+        if (MicroBit.getInstance().isMicrobit_connected()) {
+            switch (exit_step) {
+                case 1:
+                    Log.d(Constants.TAG, "Disabling Button 1 State notifications");
+                    bluetooth_le_adapter.setNotificationsState(Utility.normaliseUUID(BleAdapterService.TEMPERATURESERVICE_SERVICE_UUID), Utility.normaliseUUID(BleAdapterService.TEMPERATURE_CHARACTERISTIC_UUID), false);
+                    break;
+                default:
+                    finish();
+                    try {
+                        unbindService(mServiceConnection);
+                    } catch (Exception e) {
+                    }
+
+            }
+        } else {
+            finish();
+            try {
+                unbindService(mServiceConnection);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_magnetometer, menu);
+        return true;
+    }
+
+    // Service message handler�//////////////////
+    private Handler mMessageHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            Bundle bundle;
+            String service_uuid = "";
+            String characteristic_uuid = "";
+            String descriptor_uuid = "";
+            byte[] b = null;
+            TextView value_text = null;
+
+            switch (msg.what) {
+                case BleAdapterService.GATT_DESCRIPTOR_WRITTEN:
+                    Log.d(Constants.TAG, "Handler received descriptor written result");
+                    bundle = msg.getData();
+                    service_uuid = bundle.getString(BleAdapterService.PARCEL_SERVICE_UUID);
+                    characteristic_uuid = bundle.getString(BleAdapterService.PARCEL_CHARACTERISTIC_UUID);
+                    descriptor_uuid = bundle.getString(BleAdapterService.PARCEL_DESCRIPTOR_UUID);
+                    b = bundle.getByteArray(BleAdapterService.PARCEL_VALUE);
+                    Log.d(Constants.TAG, "descriptor " + descriptor_uuid + " of characteristic " + characteristic_uuid + " of service " + service_uuid.toString() + " written OK:0x" + Utility.byteArrayAsHexString(b));
+                    Log.d(Constants.TAG,"exiting="+exiting);
+                    if (exiting) {
+                        shutdownSteps();
+                    }
+                    break;
+
+                case BleAdapterService.NOTIFICATION_OR_INDICATION_RECEIVED:
+                    bundle = msg.getData();
+                    service_uuid = bundle.getString(BleAdapterService.PARCEL_SERVICE_UUID);
+                    characteristic_uuid = bundle.getString(BleAdapterService.PARCEL_CHARACTERISTIC_UUID);
+                    b = bundle.getByteArray(BleAdapterService.PARCEL_VALUE);
+                    byte temperature = b[0];
+                    float fTemperature = temperature;
+                    Log.d(Constants.TAG, "Value=" + Utility.byteArrayAsHexString(b));
+                    if (characteristic_uuid.equalsIgnoreCase((Utility.normaliseUUID(BleAdapterService.TEMPERATURE_CHARACTERISTIC_UUID)))) {
+                        Log.d(Constants.TAG, "Temperature received: " + temperature);
+                        fTemperature = (fTemperature * ((float) 9/5) + 32);
+                        Log.d(Constants.TAG, "Temperature converted: " + temperature);
+                        TextView temp = (TextView) TemperatureActivity.this.findViewById(R.id.temperature);
+                        if(fahr) {
+                            temp.setText("" + fTemperature);
+                        }else {
+                            temp.setText("" + temperature);
+                        }
+                    }
+                    break;
+                case BleAdapterService.MESSAGE:
+                    bundle = msg.getData();
+                    String text = bundle.getString(BleAdapterService.PARCEL_TEXT);
+                    showMsg(Utility.htmlColorRed(text));
+            }
+        }
+    };
+
+    private void showMsg(final String msg) {
+        Log.d(Constants.TAG, msg);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ((TextView) TemperatureActivity.this.findViewById(R.id.message)).setText(Html.fromHtml(msg));
+            }
+        });
+    }
+
+    @Override
+    public void connectionStatusChanged(boolean connected) {
+        if (connected) {
+            showMsg(Utility.htmlColorGreen("Connected"));
+        } else {
+            showMsg(Utility.htmlColorRed("Disconnected"));
+        }
+    }
+
+    @Override
+    public void serviceDiscoveryStatusChanged(boolean new_state) {
+
+    }
+}
